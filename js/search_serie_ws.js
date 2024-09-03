0e99c9625c5b116b0cc0c8ce4bbae8f389d1ddb1
fsfeds95 <144530359+fsfeds95@users.noreply.github.com>
2024-09-03 13:16:23 -0400

    Actualizar search_serie_ws.js

--- js/search_serie_ws.js
+++ js/search_serie_ws.js
@@ -62,12 +62,12 @@ function displaySeriesList(series) {
  <div class="movie-card__header" style="background-image: url(${popBackdropFit})">
   <span class="movie-card_genre">ID: ${id}</span>
   <span class="movie-card_genre">
-   <a href="https://bfc30010-7323-4c16-9b06-e31ddf53c427.e1-us-cdp-2.choreoapps.dev/p?url=${popPosterFat}" target="_blank">
+   <a href="https://095a2517-5733-4377-8631-a3e04ed221e8.e1-us-cdp-2.choreoapps.dev/p?url=${popPosterFat}" target="_blank">
     Poster
    </a>
   </span>
   <span class="movie-card_genre">
-   <a href="https://bfc30010-7323-4c16-9b06-e31ddf53c427.e1-us-cdp-2.choreoapps.dev/b?url=${popBackdropFat}" target="_blank">
+   <a href="https://095a2517-5733-4377-8631-a3e04ed221e8.e1-us-cdp-2.choreoapps.dev/bSeries?b=${popBackdropFat}&p=${popPosterFat}" target="_blank">
     Backdrop
    </a>
   </span>
